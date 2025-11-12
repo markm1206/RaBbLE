0602f7ff3f47d343d374f85b223fe662028a247f
Mark McConachie <markm1206@gmail.com>
2025-11-11 19:41:16 -0600

    Eye blink logic finished.

--- eye.py
+++ eye.py
@@ -24,7 +24,7 @@ class Eye:
         # Blinking state
         self.last_blink_time = 0
         self.blink_interval = 1000  # Default blink interval
-        self.blink_close_duration = 150
+        self.blink_close_duration = 75  # Faster blink
         self.blink_open_duration = 150
         self.blink_pause_duration = 50
         self.blink_state = "IDLE"
@@ -61,12 +61,15 @@ class Eye:
         current_time = pygame.time.get_ticks()
 
         # Define resting and target positions for the blink
+        # Precise target_y calculations for tangent eyelid movement
         if self.eyelid_position == 'top':
             resting_y = self.y - self.radius - self.ellipse_height + self.overlap_amount
-            target_y = self.y + self.radius - self.ellipse_height
+            # Top edge of ellipse intersects bottom edge of eye circle
+            target_y = self.y + self.radius
         else:  # bottom
             resting_y = self.y + self.radius - self.overlap_amount
-            target_y = self.y - self.radius
+            # Bottom edge of ellipse tangent to top edge of eye circle
+            target_y = (self.y - self.radius) - self.ellipse_height
 
         current_ellipse_y = resting_y
 
@@ -79,13 +82,28 @@ class Eye:
             blink_progress = min(1, (current_time - self.blink_start_time) / self.blink_open_duration)
             current_ellipse_y = target_y + (resting_y - target_y) * blink_progress
 
-        # Draw the eyelid and its mask
+        # 1. Draw the eyelid ellipse
         ellipse_rect = pygame.Rect(self.x - (self.ellipse_width // 2), current_ellipse_y, self.ellipse_width, self.ellipse_height)
         pygame.draw.ellipse(screen, self.color, ellipse_rect)
-        
-        # Draw the eye on top
-        pygame.draw.circle(screen, self.background_color, (self.x, self.y), self.radius, 0)
-        pygame.draw.circle(screen, self.color, (self.x, self.y), self.radius, 4)
+
+        # 2. Draw the circle (ring and background center)
+        pygame.draw.circle(screen, self.background_color, (self.x, self.y), self.radius, 0) # Background center
+        pygame.draw.circle(screen, self.color, (self.x, self.y), self.radius, 4) # Ring outline
+
+        # 3. Draw the occlusion rectangle
+        # Refined occlusion rectangle positioning and size for full, symmetrical coverage with margin
+        buffer_margin = 5
+        if self.eyelid_position == 'top':
+            # Rectangle attached to the top of the top ellipse and extends upwards
+            rect_y = self.y - self.radius - buffer_margin # Start above the top of the eye circle
+            rect_height = current_ellipse_y - rect_y # Extend from rect_y to the top of the ellipse
+            occlusion_rect = pygame.Rect(self.x - (self.ellipse_width // 2), rect_y, self.ellipse_width, rect_height)
+        else:  # bottom
+            # Rectangle attached to the bottom of the bottom ellipse and extends downwards
+            rect_y = current_ellipse_y + self.ellipse_height
+            rect_height = (self.y + self.radius) - rect_y + buffer_margin
+            occlusion_rect = pygame.Rect(self.x - (self.ellipse_width // 2), rect_y, self.ellipse_width, rect_height)
+        pygame.draw.rect(screen, self.background_color, occlusion_rect)
 
     def set_blink_interval(self, interval):
         """Set the blink interval in milliseconds."""
